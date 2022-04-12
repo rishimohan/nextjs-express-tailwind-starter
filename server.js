cbfed24473e4e61d7aecab9e94ad4e62831735c2
Rishi <iamrishi.ms@gmail.com>
2022-04-12 10:43:24 +0530

    Test API route

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
+  server.get("/api/shows", (req, res) => {
+    return res.end("Henlo world 11")
+  });
+
   server.all('*', (req, res) => {
     return handle(req, res)
   })
+
 
   server.listen(port, (err) => {
     if (err) throw err
